228c9346cfb4252a53514e12c7a8db9b9830ded5
Adalberto Alonso Pinedo <apinedo82@gmail.com>
2023-02-22 19:05:58 -0500

    Mi Primer Commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/baseultima',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}, (err) =>{
+    if (err){
+        console.log(err)
+    } else {
+        console.log('conectado')
+    }
+}
+)
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Escuchando puerto 3000');
+})
